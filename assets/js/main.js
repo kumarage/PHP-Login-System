85f5dd126e9269312558c7999ba090fcde4a78bb
kumarage <pubudukuma@gmail.com>
2018-10-04 03:11:19 +0530

    Added reister AJAX

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,3 +58,63 @@ $(document)
 
 	return false;
 })
+//for login file
+.on("submit", "form.js-login", function(event){
+	//below code prevents us from taking out of the page
+	event.preventDefault(); 
+
+	var _form = $(this);
+	var _error = $(".js-error",_form);
+
+	var dataObj = {
+		email: $("input[type='email']",_form).val(),
+		password: $("input[type='password']",_form).val()
+	}
+
+	if(dataObj.email.length<6) {
+		_error
+			.text("Please enter a valid email")
+			.show();
+		return false;
+	} else if (dataObj.password.length<8){
+		_error
+			.text("Password is not long enough")
+			.show();
+		return false;	
+	}
+
+	//Assumiing a user get this far we can start the ajax process
+
+	_error.hide();
+
+	$.ajax({
+		type: 'POST',
+		url: '/php_login_course/ajax/login.php',
+		data: dataObj,
+		dataType:'json',
+		async: true
+	})
+	.done(function ajaxDone(data) {
+		// whatever data is
+		console.log(data);
+		if(data.redirect !== undefined){	
+			window.location.href = data.redirect;
+		} else if (data.error !== undefined){
+			_error
+			.text(data.error)
+			.show();
+		}
+
+		alert(data.name);
+	})
+	.fail(function ajaxFailed(e) {
+		// This failed
+		console.log(e);
+	})
+	.always(function ajaxAlwaysDoThis(data){
+		// Always do
+		console.log('Always');
+	})
+
+	return false;
+})
